ef2970b5fa2843eefcbc199c1f305a3a702453a5
yassinmftah <yassinmaftah06@gmail.com>
2026-01-18 18:49:05 +0100

    class StudentMember that extends from abstruct class Member

---
+++ src/Models/StudentMember.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class StudentMember extends Member 
+{
+    public function getLoanDurationDays() {return 14; }
+    public function getMaxBorrowLimit() {return 3;}
+    public function getDailyLateFee() {return 0.50;}
+}
