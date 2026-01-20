c9142589fdae67a5bb582bdfd1c4202aa4e4ded2
yassinmftah <yassinmaftah06@gmail.com>
2026-01-20 09:19:55 +0100

    change name of method LoanDuration

--- src/Models/FacultyMember.php
+++ src/Models/FacultyMember.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class FacultyMember extends Member 
 {
-    public function getLoanDurationDays() {return 30;}
+    public function LoanDuration() {return 30;}
     public function getMaxBorrowLimit() {return 10;}
     public function getDailyLateFee() {return 0.25;}
 }
